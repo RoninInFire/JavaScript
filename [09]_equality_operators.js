0550cc991c9e0e669b7a0e9a1932928393ec6b62
Rustam JS <aflyatunov96@gmail.com>
2026-01-28 19:22:19 +0300

    Equality operators

---
+++ [09]_equality_operators.js
@@ -0,0 +1,14 @@
+const secretNumber = '7';
+
+if (Number(secretNumber) === 7) {
+    console.log('Угадал строго')
+}
+
+if (Number(secretNumber) == 7) {
+    console.log('Угадал не строго')
+}
+
+const q = Number(prompt('Введите число'));
+if (q ===7) {
+    console.log('!');
+}
