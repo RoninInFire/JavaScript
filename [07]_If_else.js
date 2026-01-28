c1f13ab57da54ccab56f8ab7e35e1a18f753b720
Rustam JS <aflyatunov96@gmail.com>
2026-01-28 14:28:53 +0300

    If Else

---
+++ [07]_If_else.js
@@ -0,0 +1,12 @@
+const money = 100;
+const canBuy = money > 50;
+
+if (money > 50) {
+    console. log('Может купить наш продукт');
+} else if (money > 5) {
+    console.log('Куплен mini-продукт');
+} else {
+    console. log('Недостаточно средств');
+}
+
+console. log('Итог');
